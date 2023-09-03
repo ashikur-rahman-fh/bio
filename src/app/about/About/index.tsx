ce4fd3e7dcce37dc6c6dc7a95bb78ace2c7ce806
Ashikur Rahman <ashikurrahmanvar@gmail.com>
2023-09-03 11:44:23 +0000

    Add basic info

--- src/app/about/About/index.tsx
+++ src/app/about/About/index.tsx
@@ -3,7 +3,22 @@ import Image from "next/image";
 
 import ashikurDp from "./prof-dp.png";
 
+import { BASIC_INFO } from "./constants";
+
 const About = () => {
+  const renderBasicInfo = () => {
+    const infoLenght = BASIC_INFO.length;
+    return BASIC_INFO.map((info, index) => {
+      const columnFull = index === infoLenght - 1 && infoLenght % 2 === 1;
+      const className = columnFull ? "md:col-span-2" : "";
+      return (
+        <p className={className} key={index.toString()}>
+          {info}
+        </p>
+      );
+    });
+  };
+
   return (
     <section className="mx-2 my-2 md:mx-32 lg:mx-[10vw]">
       <div className="relative float-right my-2 ml-8 hidden overflow-hidden rounded-full border-2 border-gray-100 shadow-sm shadow-gray-500 lg:inline-block">
@@ -26,17 +41,8 @@ const About = () => {
           </h2>
         </div>
 
-        <div className="grid grid-cols-1 gap-8 text-justify md:grid-cols-2 md:gap-16">
-          <p className="p-2 shadow-sm shadow-gray-300">
-            Expertise in Data Structures and Algorithms! Solved more than 2000
-            programming problems and participated in more than 100 programming
-            contests.
-          </p>
-          <p className="p-2 shadow-sm shadow-gray-300 md:p-4">
-            Mentored and guide hundreds of students to become successful in
-            programming contests and programming and software development
-            careers.
-          </p>
+        <div className="grid grid-cols-1 gap-4 text-justify md:grid-cols-2 md:gap-8 [&>p]:rounded-md [&>p]:bg-green-200 [&>p]:p-4 [&>p]:shadow-md [&>p]:shadow-green-200">
+          {renderBasicInfo()}
         </div>
       </div>
     </section>
