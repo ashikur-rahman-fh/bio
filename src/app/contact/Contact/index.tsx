cb7022f07674ef357938067a5c90e6e3e6c64fd0
Ashikur Rahman <ashikurrahmanvar@gmail.com>
2023-10-06 11:24:13 -0400

    Impove SSR for contact tab

--- src/app/contact/Contact/index.tsx
+++ src/app/contact/Contact/index.tsx
@@ -1,12 +1,9 @@
 "use client";
 
 import React from "react";
-
 import { Button, Input, Textarea } from "@material-tailwind/react";
-import Gmail from "@/app/components/LogoComponent/Gmail";
-import LinkedIn from "@/app/components/LogoComponent/LinkedIn";
 
-import { CONTACT_INFO } from "@/app/constants";
+import ContactInfo from "./ContactInfo";
 
 import "./style.css";
 
@@ -34,27 +31,7 @@ const Contact = () => {
           <Button>Send</Button>
         </form>
 
-        <div className="items-left flex flex-col justify-center gap-4 pl-11">
-          <h1 className="flex items-center gap-2">
-            <Gmail className="small-gmail" />
-            <a
-              className="underline underline-offset-2 "
-              href={`mailto: ${CONTACT_INFO?.EMAIL}`}
-            >
-              {CONTACT_INFO?.EMAIL}
-            </a>
-          </h1>
-
-          <h1 className="flex items-center gap-2">
-            <LinkedIn className="small-linkedin" />
-            <a
-              className="underline underline-offset-2"
-              href="linkedin.com/in/ashikur-5h/"
-            >
-              linkedin.com/in/ashikur-5h/
-            </a>
-          </h1>
-        </div>
+        <ContactInfo />
       </div>
     </section>
   );
