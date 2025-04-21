59e36b2d6db49f125722ecee372e10699ffd319d
Ashikur Rahman <ashikurrahmanvar@gmail.com>
2025-04-20 20:42:34 -0400

    Fix typo and lint errors

--- src/app/experience/WorkExperience/index.tsx
+++ src/app/experience/WorkExperience/index.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import { IOrganization } from "./types";
 import { Experience } from "./constants";
 
-
 const Organization = ({ organization } : { organization: IOrganization }) => {
   return (
     <React.Fragment>
@@ -12,7 +11,7 @@ const Organization = ({ organization } : { organization: IOrganization }) => {
         <Image
           className="border-blue-200 border-2 border-solid h-24 w-40 p-2 m-4"
           src={organization.logoLink}
-          alt="Next.js Logo"
+          alt="workplace logo"
           width={100}
           height={100}
           priority
@@ -26,9 +25,14 @@ const Organization = ({ organization } : { organization: IOrganization }) => {
 const WorkExperience = () => {
   return (
     <React.Fragment>
-      {Experience.map(experience => (
-        <Organization key={experience.organization.name} organization={experience.organization} />
-      ))}
+      {Experience.map((experience) => {
+        return (
+          <Organization
+            key={experience.organization.name}
+            organization={experience.organization}
+          />
+        );
+      })}
     </React.Fragment>
   );
 };
--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Metadata } from "next";
+
 import WorkExperience from "./WorkExperience";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Work Experience - Ashikur Rahman",
+    description:
+      "Ashikur has been working as a software developer over three (3) " +
+      "years. " +
+      "Currently working at Nokia.",
+    keywords:
+      "Software Developer, 3 years, three years, OOP, Object oriented " +
+      "programming, design patterns, micro service architecture, embedded " +
+      "software development, linux environment, gRPC, REST, Git, Gerrit",
+  };
+}
 
 const WorkExperiencePage = () => {
   return (
