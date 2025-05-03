509c92211549358634f1b6d3fc781cb0775118a3
Ashikur Rahman <ashikurrahmanvar@gmail.com>
2025-05-03 09:36:56 -0400

    Automate tests and copyright text

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -3,11 +3,9 @@ import React from "react";
 import { CONTACT_INFO } from "@/app/constants";
 
 const Footer = () => {
-  const startDate = "2023";
-
   return (
     <footer className="text-slate-500 mt-10 text-center text-sm">
-      &copy; Copyright {startDate}-{new Date().getFullYear()}{" "}
+      &copy; Copyright {new Date().getFullYear()}{" "}
       {CONTACT_INFO.name}
     </footer>
   );
--- test.sh
+++ test.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# run lint
+echo "Running lint"
+npm run lint
+
+if [ $? -ne 0 ]; then
+  echo "Lint failed. Exiting..."
+  exit
+else
+  echo "Lint passed"
+fi
+
 # pre test:
 # restore babel config from backup
 mv babel.config.js.bak babel.config.js
