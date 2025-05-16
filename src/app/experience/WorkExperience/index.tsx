85547859fad70df5ccee9de351cb2b7c12aaad9c
Ashikur Rahman <ashikurrahmanvar@gmail.com>
2025-05-16 11:27:29 -0400

    Add work experience headline

--- src/app/experience/WorkExperience/index.tsx
+++ src/app/experience/WorkExperience/index.tsx
@@ -4,10 +4,21 @@ import Organization from "./Organization";
 
 import { Experience } from "./constants";
 
+const HeadLine = () => {
+  return (
+    <React.Fragment>
+      <h1 className="text-2xl font-bold uppercase">
+        Writing production code since 2020.<br/>
+        Every year since has been a deeper dive into building smarter and faster systems.
+      </h1>
+    </React.Fragment>
+  );
+}
 
 const WorkExperience = () => {
   return (
     <React.Fragment>
+      <HeadLine />
       {Experience.map((experience) => {
         return (
           <Organization
